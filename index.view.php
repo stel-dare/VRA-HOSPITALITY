a48e666ecfc62e411d4055a3d99f9e59497fe94e
Stel Dare <stelladare2@gmail.com>
2020-07-04 00:32:18 +0000

    Create Header - Info section

--- index.view.php
+++ index.view.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Volta River Authority | Hospitality</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" integrity="sha512-xA6Hp6oezhjd6LiLZynuukm80f8BoZ3OpcEYaqKoCV3HKQDrYjDE1Gu8ocxgxoXmwmSzM4iqPvCsOkQNiu41GA==" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <h1><?= "Hello World!" ?></h1>
+    <header>
+        <div class="header-info">
+            <div class="location-info">
+                <span><i class="fas fa-map-marker-alt"></i> ELectro Volta House, Ministries - Accra</span>
+                <span><i class="fas fa-map-marker-alt"></i> Digital Address: GA-145-7445</span>
+                <span><i class="far fa-envelope"></i>corpcomm@vra.com</span>
+            </div>
+            <div class="announcement-info">
+                <span>TEMPORARY SUSPENSION OF VISITS TO OUR PLANTS</span>
+            </div>
+        </div>
+    </header>
     
 </body>
 </html>
